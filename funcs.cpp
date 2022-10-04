52fe93a5064f3dae0b36a7f329f0c6de60688fee
Shenghao Dong <shenghaodong18@gmail.com>
2022-10-03 23:20:42 -0400

    finished part a to c

--- funcs.cpp
+++ funcs.cpp
@@ -3,3 +3,52 @@
 
 // add functions here
 
+//Task A. Is divisible?
+bool isDivisibleBy(int n, int d){
+    if(n % d){
+        return true;
+    }
+    return false;
+};
+
+//Task B. Is a prime?
+bool isPrime(int n){
+    for(int i = 2; i < n-1; i++){
+        if(n%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+//Task C. Next prime
+int nextPrime(int n){
+    int counter = n;
+    while(true){
+        counter++;
+        if(isPrime(counter) == 1){
+            break;
+        }
+    }
+    return counter;
+}
+
+
+//Task D. Count primes in range
+int countPrimes(int a, int b){
+    
+}
+
+
+
+//Task E. Is a twin prime?
+
+
+
+//Task F. Next twin prime
+
+
+
+//Task G. Largest twin prime in range
+
--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@
 
 int main()
 {
+  // 1 = true, 0 = false
+  std::cout << isPrime(4) << std::endl;
+  std::cout << nextPrime(13) << std::endl;
   return 0;
 }
