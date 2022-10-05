2b72f509ac618a98ec369f378a52138cd530893b
Shenghao Dong <shenghaodong18@gmail.com>
2022-10-05 19:58:34 -0400

    added output

--- main.cpp
+++ main.cpp
@@ -5,8 +5,40 @@
 
 int main()
 {
-  // 1 = true, 0 = false
-  std::cout << isPrime(4) << std::endl;
+  std::cout << "Task A. Is divisible?" << std::endl;
+  std::cout << isDivisibleBy(10, 2) << std::endl;
+  std::cout << isDivisibleBy(39, 2) << std::endl;
+  std::cout << "\n";
+
+  std::cout << "Task B. Is a prime?" << std::endl;
+  std::cout << isPrime(3) << std::endl;
+  std::cout << isPrime(10) << std::endl;
+  std::cout << "\n";
+
+  std::cout << "Task C. Next prime" << std::endl;
+  std::cout << nextPrime(21) << std::endl;
   std::cout << nextPrime(13) << std::endl;
+  std::cout << "\n";
+
+  std::cout << "Task D. Count primes in range" << std::endl;
+  std::cout << countPrimes(5, 92) << std::endl;
+  std::cout << countPrimes(27, 39) << std::endl;
+  std::cout << "\n";
+
+  std::cout << "Task E. Is a twin prime?" << std::endl;
+  std::cout << isTwinPrime(3) << std::endl;
+  std::cout << isTwinPrime(28) << std::endl;
+  std::cout << "\n";
+
+  std::cout << "Task F. Next twin prime" << std::endl;
+  std::cout << nextTwinPrime(67) << std::endl;
+  std::cout << nextTwinPrime(29) << std::endl;
+  std::cout << "\n";
+
+  std::cout << "Task G. Largest twin prime in range" << std::endl;
+  std::cout << largestTwinPrime(9, 21) << std::endl;
+  std::cout << largestTwinPrime(32, 65) << std::endl;
+  std::cout << "\n";
+
   return 0;
 }
